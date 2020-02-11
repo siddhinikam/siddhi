fcf0b76660957f1769395daef40b27b7d9e9f359
siddhinikam <60913569+siddhinikam@users.noreply.github.com>
2020-02-11 12:13:11 +0530

    exam

---
+++ ReverseString.java
@@ -0,0 +1,23 @@
+package exam;
+class ReverseString
+{ 
+
+ void reverse(String str) 
+ { 
+     if ((str==null)||(str.length() <= 1)) 
+        System.out.println(str); 
+     else
+     { 
+         System.out.print(str.charAt(str.length()-1)); 
+         reverse(str.substring(0,str.length()-1)); 
+     } 
+ } 
+   
+
+ public static void main(String[] args)  
+ { 
+     String str = "PatilHande"; 
+     ReverseString obj = new ReverseString(); 
+     obj.reverse(str); 
+ }     
+}
---
+++ SumArray.java
@@ -0,0 +1,17 @@
+package exam;
+
+	public class SumArray {
+
+	    public static void main(String[] args) {
+
+	        int number = 5;
+	        long fact = 1;
+	        for(int i = 1; i <= number; i++)
+	        {
+	            fact = fact * i;
+	        }
+	        System.out.println("Factorial of "+number+" is: "+fact);
+	    }
+	}
+
+
